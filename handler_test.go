d6c4828ed37442ef16ab0e2f9f9560a4bf3bc515
Carl Tablante <carl@guavapass.com>
2017-07-10 22:27:02 +0800

    refactor more tests

--- handler_test.go
+++ handler_test.go
@@ -22,21 +22,21 @@ func TestRiddleHandler(t *testing.T) {
 
 	request := strings.NewReader(`{
   "session": {
-    "sessionId": "SessionId.7098bcf8-9994-4bbf-8ae7-b41d85723a7d",
+    "sessionId": "SessionId.629edaf6-af16-4433-a5d8-6b8c3f7dac71",
     "application": {
       "applicationId": "amzn1.ask.skill.3aebac54-38a0-4dd3-9f17-4942972e4136"
     },
     "attributes": {},
     "user": {
-      "userId": "amzn1.ask.account.AH2Y4V7T4UXBMQEQKKIV7WMMZKVOBMGGGKWGVES3KAHDNPUOD6BMR3WD3ZL2RFT6VD47DNMGGKLG4XKAYYLBHEQ2TAKJ5PA525SKS3GZOMJC7PQZHIYLMCDHCMTDOV6AKPLZWPAQN6HJ5VZ4RGRIUBB7FK7TRO72T6BTIFJH3NXJM2P6JBKWFKJ5SKQI4LDLJYMXX2T6BZSRCDA"
+      "userId": "amzn1.ask.account.AGDAHU5UOXOY42WWBLIASG6S3EHWDFISASJENTA4B4J3N5GJSL4ZESBT65MHGGRQLD3GFWSUTCVIC2D3SA66RHLTENLW3EKPOC4F6WKUWVGGK5EEZRBVDU5E63IMFSGDNZESQU3S6AFK7EEHWUQYLPL76WFYAZUBHHXJXVQFR2CCDWFTILROASMQFXGTPWZ6Q5YGAU5JXO7AAJY"
     },
     "new": true
   },
   "request": {
     "type": "IntentRequest",
-    "requestId": "EdwRequestId.1c73c114-22e7-4cf8-b8b1-720bc2123122",
+    "requestId": "EdwRequestId.aba6e206-85d7-4131-8fad-f662a9a39286",
     "locale": "en-US",
-    "timestamp": "2017-07-08T14:41:46Z",
+    "timestamp": "2017-07-10T14:23:39Z",
     "intent": {
       "name": "AskRiddle",
       "slots": {}
@@ -59,21 +59,21 @@ func TestRepeatRiddleIntentNoSession(t *testing.T) {
 
 	request := strings.NewReader(`{
   "session": {
-    "sessionId": "SessionId.7098bcf8-9994-4bbf-8ae7-b41d85723a7d",
+    "sessionId": "SessionId.6e7bf06d-f169-4427-ada7-7e11b51a6598",
     "application": {
       "applicationId": "amzn1.ask.skill.3aebac54-38a0-4dd3-9f17-4942972e4136"
     },
     "attributes": {},
     "user": {
-      "userId": "amzn1.ask.account.AH2Y4V7T4UXBMQEQKKIV7WMMZKVOBMGGGKWGVES3KAHDNPUOD6BMR3WD3ZL2RFT6VD47DNMGGKLG4XKAYYLBHEQ2TAKJ5PA525SKS3GZOMJC7PQZHIYLMCDHCMTDOV6AKPLZWPAQN6HJ5VZ4RGRIUBB7FK7TRO72T6BTIFJH3NXJM2P6JBKWFKJ5SKQI4LDLJYMXX2T6BZSRCDA"
+      "userId": "amzn1.ask.account.AGDAHU5UOXOY42WWBLIASG6S3EHWDFISASJENTA4B4J3N5GJSL4ZESBT65MHGGRQLD3GFWSUTCVIC2D3SA66RHLTENLW3EKPOC4F6WKUWVGGK5EEZRBVDU5E63IMFSGDNZESQU3S6AFK7EEHWUQYLPL76WFYAZUBHHXJXVQFR2CCDWFTILROASMQFXGTPWZ6Q5YGAU5JXO7AAJY"
     },
     "new": true
   },
   "request": {
     "type": "IntentRequest",
-    "requestId": "EdwRequestId.1c73c114-22e7-4cf8-b8b1-720bc2123122",
+    "requestId": "EdwRequestId.a9b03014-6d6e-45de-a9c1-c24c58ba336b",
     "locale": "en-US",
-    "timestamp": "2017-07-08T14:41:46Z",
+    "timestamp": "2017-07-10T14:24:42Z",
     "intent": {
       "name": "RepeatRiddle",
       "slots": {}
@@ -100,23 +100,23 @@ func TestRepeatRiddleWithSession(t *testing.T) {
 	assert := assert.New(t)
 	request := strings.NewReader(`{
   "session": {
-    "sessionId": "SessionId.7098bcf8-9994-4bbf-8ae7-b41d85723a7d",
+    "sessionId": "SessionId.6e7bf06d-f169-4427-ada7-7e11b51a6598",
     "application": {
       "applicationId": "amzn1.ask.skill.3aebac54-38a0-4dd3-9f17-4942972e4136"
     },
     "attributes": {
-			"answer": "man"
-		},
-    "user": {
-      "userId": "amzn1.ask.account.AH2Y4V7T4UXBMQEQKKIV7WMMZKVOBMGGGKWGVES3KAHDNPUOD6BMR3WD3ZL2RFT6VD47DNMGGKLG4XKAYYLBHEQ2TAKJ5PA525SKS3GZOMJC7PQZHIYLMCDHCMTDOV6AKPLZWPAQN6HJ5VZ4RGRIUBB7FK7TRO72T6BTIFJH3NXJM2P6JBKWFKJ5SKQI4LDLJYMXX2T6BZSRCDA"
+      "answer": "heroine"
     },
-    "new": true
+    "user": {
+      "userId": "amzn1.ask.account.AGDAHU5UOXOY42WWBLIASG6S3EHWDFISASJENTA4B4J3N5GJSL4ZESBT65MHGGRQLD3GFWSUTCVIC2D3SA66RHLTENLW3EKPOC4F6WKUWVGGK5EEZRBVDU5E63IMFSGDNZESQU3S6AFK7EEHWUQYLPL76WFYAZUBHHXJXVQFR2CCDWFTILROASMQFXGTPWZ6Q5YGAU5JXO7AAJY"
+    },
+    "new": false
   },
   "request": {
     "type": "IntentRequest",
-    "requestId": "EdwRequestId.1c73c114-22e7-4cf8-b8b1-720bc2123122",
+    "requestId": "EdwRequestId.d8c807dd-f9a3-41d1-9d76-0200b4246436",
     "locale": "en-US",
-    "timestamp": "2017-07-08T14:41:46Z",
+    "timestamp": "2017-07-10T14:25:37Z",
     "intent": {
       "name": "RepeatRiddle",
       "slots": {}
@@ -137,7 +137,7 @@ func TestRepeatRiddleWithSession(t *testing.T) {
 
 	alexaResp := alexaskill.AlexaResponse{}
 	json.Unmarshal(rr.Body.Bytes(), &alexaResp)
-	expect := riddles.GetRiddle("man")
+	expect := riddles.GetRiddle("heroine")
 
 	assert.Equal(expect, alexaResp.Response.OutputSpeech.Text)
 }
@@ -191,33 +191,34 @@ func TestAnswerRiddleWrongAnswer(t *testing.T) {
 	expect := "Sorry, manner is not the answer. Try again"
 
 	assert.Equal(expect, alexaResp.Response.OutputSpeech.Text)
+	assert.False(alexaResp.Response.ShouldEndSession)
 }
 
 func TestAnswerRiddleNoSession(t *testing.T) {
 	assert := assert.New(t)
 	request := strings.NewReader(`{
   "session": {
-    "sessionId": "SessionId.8a422ce6-3243-46ee-afa9-6c8f33dabe7c",
+    "sessionId": "SessionId.6e7bf06d-f169-4427-ada7-7e11b51a6598",
     "application": {
       "applicationId": "amzn1.ask.skill.3aebac54-38a0-4dd3-9f17-4942972e4136"
     },
     "attributes": {},
     "user": {
-      "userId": "amzn1.ask.account.AH2Y4V7T4UXBMQEQKKIV7WMMZKVOBMGGGKWGVES3KAHDNPUOD6BMR3WD3ZL2RFT6VD47DNMGGKLG4XKAYYLBHEQ2TAKJ5PA525SKS3GZOMJC7PQZHIYLMCDHCMTDOV6AKPLZWPAQN6HJ5VZ4RGRIUBB7FK7TRO72T6BTIFJH3NXJM2P6JBKWFKJ5SKQI4LDLJYMXX2T6BZSRCDA"
+      "userId": "amzn1.ask.account.AGDAHU5UOXOY42WWBLIASG6S3EHWDFISASJENTA4B4J3N5GJSL4ZESBT65MHGGRQLD3GFWSUTCVIC2D3SA66RHLTENLW3EKPOC4F6WKUWVGGK5EEZRBVDU5E63IMFSGDNZESQU3S6AFK7EEHWUQYLPL76WFYAZUBHHXJXVQFR2CCDWFTILROASMQFXGTPWZ6Q5YGAU5JXO7AAJY"
     },
     "new": false
   },
   "request": {
     "type": "IntentRequest",
-    "requestId": "EdwRequestId.4c232378-091f-4f4a-85d8-879c74d8b083",
+    "requestId": "EdwRequestId.b269381d-5e33-47eb-b08f-9d023f286a51",
     "locale": "en-US",
-    "timestamp": "2017-07-09T07:36:49Z",
+    "timestamp": "2017-07-10T14:27:56Z",
     "intent": {
       "name": "AnswerRiddle",
       "slots": {
         "RiddleAnswer": {
-          "name": "ANSWER_LIST",
-          "value": "manner"
+          "name": "RiddleAnswer",
+          "value": "man"
         }
       }
     }
